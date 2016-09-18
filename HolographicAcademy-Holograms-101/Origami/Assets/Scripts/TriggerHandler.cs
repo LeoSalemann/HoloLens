458eba9c7c88307187afdf41e5561f273428a6df
LeoSalemann <leo.salemann@me.com>
2016-09-18 11:27:14 -0700

    Working wiht colision boxespartial success
    got a capsule, but no change

---
+++ HolographicAcademy-Holograms-101/Origami/Assets/Scripts/TriggerHandler.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerHandler : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+    void OnTriggerEnter(Collider other)
+    {
+        other.gameObject.SetActiveRecursively(true);
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        other.gameObject.SetActiveRecursively(false); ;
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
